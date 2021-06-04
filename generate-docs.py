1f7a54f62384bf141636eb8faeda15887f7d68d1
Robert Brook <robertbrook@fastmail.fm>
2021-06-04 17:10:19 +0100

    Testing writing files in action.

--- generate-docs.py
+++ generate-docs.py
@@ -1,3 +1,20 @@
+import os
+import rdflib
+from datetime import datetime
 from pathlib import Path
-result = list(Path(".").rglob("*.ttl"))
-print(result)
+
+htmldir = "./meta/html/"
+
+for ttlpath in list(Path(".").rglob("*.ttl")): 
+
+    try:
+        os.makedirs(htmldir + str(ttlpath.parent))
+    except FileExistsError:
+        pass
+
+    htmlfile = open(htmldir + str(ttlpath.parent) + "/" + ttlpath.stem + ".html", "w")
+    htmlfile.write(str(ttlpath))
+    htmlfile.write(str(datetime.now()))
+    htmlfile.close()
+
+
