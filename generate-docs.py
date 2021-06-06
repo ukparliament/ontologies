b3c4d93c810a4ee45d59a16d91b1c94fc7ddbe8a
Robert Brook <robertbrook@fastmail.fm>
2021-06-06 21:10:42 +0100

    building

--- generate-docs.py
+++ generate-docs.py
@@ -1,8 +1,11 @@
 import os
 import rdflib
+
 from datetime import datetime
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from rdflib.namespace import RDF, FOAF, OWL, RDFS, DCTERMS
+from markupsafe import Markup
 
 env = Environment(
     loader = FileSystemLoader("templates"),
@@ -19,6 +22,94 @@ for ttlpath in list(Path(".").rglob("*.ttl")):
     
     ttlfile = open(ttlpath, "r")
     result = g.parse(data=ttlfile.read(), format="turtle")
+
+    classes = []
+
+    for s, p, o in g.triples((None, RDF.type, OWL.Class)):
+        superclasses = []
+
+        superclassobjects = g.objects(s, RDFS.subClassOf)
+        for superclassobject in superclassobjects:
+            superclasses.append(superclassobject.split("/")[-1])
+
+        h3id = g.label(s).lower().replace(" ", "-")
+        # 		  classes.append(f'<article class="class"><h3 id="{h3id}">{g.label(s)}</h3> {subClassNote}<p>{g.value(s, RDFS.comment)}</p></article>')
+        classes.append(
+            {
+                "label": g.label(s),
+                "comment": g.value(s, RDFS.comment),
+                "isDefinedBy": g.value(s, RDFS.isDefinedBy),
+                "superclasses": superclasses,
+            }
+        )
+        
+    dataproperties = []
+    
+    for s, p, o in g.triples((None, RDF.type, OWL.DatatypeProperty)):
+    
+    	dataproperties.append(
+    		{
+                "label": g.label(s),
+				"comment": g.value(s, RDFS.comment),
+                "domain": g.value(s, RDFS.domain).split("/")[-1],
+                "range": g.value(s, RDFS.range).split("#")[-1],
+            }
+        )
+
+    objectproperties = []
+
+    for s, p, o in g.triples((None, RDF.type, OWL.ObjectProperty)):
+        h3id = g.label(s).lower().replace(" ", "-")
+        domainstub = g.value(s, RDFS.domain).split("/")[-1]
+        rangestub = g.value(s, RDFS.range).split("/")[-1]
+        superpropertyv = g.value(s, RDFS.subPropertyOf)
+        if superpropertyv:
+            superproperty = g.value(s, RDFS.subPropertyOf).split("/")[-1]
+        else:
+            superproperty = ""
+
+        # 		properties.append(f'<article class="property"><h3 id="{h3id}">{g.label(s)}</h3><ul><li>{domainstub} (domain) &rarr; {g.label(s)} (property) &rarr; {rangestub} (range)</li></ul><p>{g.value(s, RDFS.comment)}</p></article>')
+        objectproperties.append(
+            {
+                "label": g.label(s),
+                "domain": domainstub,
+                "range": rangestub,
+                "superproperty": superproperty,
+                "comment": g.value(s, RDFS.comment),
+            }
+        )
+
+    for s, p, o in g.triples((None, RDF.type, OWL.Ontology)):
+        title = g.value(s, DCTERMS.title)
+        description = g.value(s, DCTERMS.description)
+        created = g.value(s, DCTERMS.created)
+        rights = g.value(s, DCTERMS.rights)
+        depiction = g.value(s, FOAF.depiction) or ""  # check
+
+    foafmakerids = []
+
+    makerobjects = []
+
+    for maker in g.objects(s, FOAF.maker):
+        foafmakerids.append(maker)
+        makerobject = {}
+        for s, p, o in g.triples((maker, None, None)):
+            makerobject["id"] = str(s)
+            makerobject["name"] = str(g.value(s, FOAF.name))
+            makerobject["homepage"] = str(g.value(s, FOAF.homepage))
+
+            makerobjects.append(makerobject)
+
+    makers = []
+    for i in makerobjects:
+        if i not in makers:
+            makers.append(i)
+
+
+ 
+
+
+
     print("PARSED\t" + str(ttlpath))
 
     try:
@@ -28,11 +119,22 @@ for ttlpath in list(Path(".").rglob("*.ttl")):
 
     htmlpath = htmldir + str(ttlpath.parent) + "/" + ttlpath.stem + ".html"
     htmlfile = open(htmlpath, "w")
-    htmlfile.write("<pre>")
-    for subject, predicate, object in g:
-        htmlfile.write("\t".join([subject, predicate, object]))
-        htmlfile.write("\n")
-    htmlfile.write("</pre>")
+    htmlfile.write(template.render(
+        title=Markup(title),
+        created=Markup(created),
+        rights=Markup(rights),
+        description=Markup(description),
+        depiction=Markup(depiction),
+        ttlurl="ttlurl",
+        classes=classes,
+        objectproperties=objectproperties,
+        namespaces=g.namespaces(),
+        makers=makers,
+        root_url="root_url",
+        imports=g.triples((None, OWL.imports, None)),
+        equivalentClasses=g.triples((None, OWL.equivalentClass, None)),
+        subClasses=g.triples((None, RDFS.subClassOf, None)),
+        dataproperties=dataproperties))
     htmlfile.close()
     print("WROTE\t" + htmlpath)
 
