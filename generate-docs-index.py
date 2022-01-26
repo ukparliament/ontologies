4fae9ecaf76ad2394dbce05b6b1f335d4948a9c2
Robert Brook <robertbrook@fastmail.fm>
2022-01-26 17:44:49 +0000

    Try listing htmlfiles

--- generate-docs-index.py
+++ generate-docs-index.py
@@ -1,3 +1,12 @@
-# stub for index generation
 
-print("run generate for index")
+import os
+
+from datetime import datetime
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from markupsafe import Markup
+from urllib.parse import urlparse
+
+htmlfiles = list(Path(".").glob("./meta/html/*/*.html"))
+
+print(htmlfiles)
