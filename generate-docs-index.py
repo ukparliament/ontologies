029bcf649498232b22e9712f4bbfd9f1911acfb2
Robert Brook <robertbrook@fastmail.fm>
2022-01-26 17:47:19 +0000

    Template needs env

--- generate-docs-index.py
+++ generate-docs-index.py
@@ -7,6 +7,14 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from markupsafe import Markup
 from urllib.parse import urlparse
 
+env = Environment(
+    loader=FileSystemLoader("templates"),
+    autoescape=select_autoescape(),
+    cache_size=0,
+    trim_blocks=True,
+    lstrip_blocks=True,
+)
+
 template = env.get_template("ontologies-index.html")
 
 htmlfiles = list(Path(".").glob("./meta/html/*/*.html"))
