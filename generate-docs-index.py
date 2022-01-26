a6c6553ac6edfdb5a6b98d4e2ffcc75d09ff5df3
Robert Brook <robertbrook@fastmail.fm>
2022-01-26 17:45:41 +0000

    Point to index template

--- generate-docs-index.py
+++ generate-docs-index.py
@@ -7,6 +7,8 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from markupsafe import Markup
 from urllib.parse import urlparse
 
+template = env.get_template("ontologies-index.html")
+
 htmlfiles = list(Path(".").glob("./meta/html/*/*.html"))
 
 print(htmlfiles)
