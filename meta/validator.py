7a76bb7241c9d9000f8b952d8293805ba5d204fa
Robert Brook <robertbrook@fastmail.fm>
2022-07-27 17:14:19 +0100

    updated validator

--- meta/validator.py
+++ meta/validator.py
@@ -1,10 +1,33 @@
 import re
 import glob
-# import rdflib
+from rdflib import Graph
 
 turtles = glob.glob("../**/*.ttl", recursive=True)
 
 targets = glob.glob("../meta/example-rdf/**/*.ttl", recursive=True)
+
+standard_namespaces = [
+    "rdf",
+    "foaf",
+    "owl",
+    "dcterms",
+    "rdfs",
+    "xsd",
+    "skos",
+    "brick",
+    "csvw",
+    "dc",
+    "dcat",
+    "dcmitype",
+    "dcam",
+    "doap",
+    "odrl",
+    "org",
+    "prof",
+    "qb",
+    "schema",
+    "sh",
+]
 
 
 def file_to_colons(thisfile):
@@ -15,15 +38,7 @@ def file_to_colons(thisfile):
             for word in words:
                 mymatch = re.match("(\w+):(\w+)", word)
                 if mymatch:
-                    if mymatch.groups()[0] not in [
-                        "rdf",
-                        "foaf",
-                        "owl",
-                        "dcterms",
-                        "rdfs",
-                        "xsd",
-                        "skos",
-                    ]:
+                    if mymatch.groups()[0] not in standard_namespaces:
                         colonlist.append(":".join(mymatch.groups()))
     return colonlist
 
@@ -39,6 +54,17 @@ flat_list = [element for sublist in allcolons for element in sublist]
 sorted_flat_list = sorted(list(set(flat_list)))
 
 for target in targets:
-	for colon in file_to_colons(target):
-		if colon not in sorted_flat_list:
-			print(f"{colon} unrecognised namepace in {target}\n")
+    g = Graph()
+    g.parse(target)
+    for subj, pred, obj in g:
+        if (subj, pred, obj) not in g:
+            print("problem with graph in {target}\n")
+    print(f"{len(g)} statements in {target}.\n")
+    if g.connected():
+        print("All nodes visited.\n")
+    for namespace in g.namespaces():
+        if "parliament.uk" in namespace[1]:
+            print(namespace[0], ":", namespace[1])
+    for colon in file_to_colons(target):
+        if colon not in sorted_flat_list:
+            print(f"\n*** {colon} not recognised in {target}\n")
