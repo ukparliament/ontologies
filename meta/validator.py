b4f0efbfd2c7e9d2e6eaf9a0ac1095c1e5cb22f0
Robert Brook <robertbrook@fastmail.fm>
2022-08-02 14:17:59 +0100

    comment out debug code

--- meta/validator.py
+++ meta/validator.py
@@ -54,17 +54,17 @@ flat_list = [element for sublist in allcolons for element in sublist]
 sorted_flat_list = sorted(list(set(flat_list)))
 
 for target in targets:
-    g = Graph()
-    g.parse(target)
-    for subj, pred, obj in g:
-        if (subj, pred, obj) not in g:
-            print("problem with graph in {target}\n")
-    print(f"{len(g)} statements in {target}.\n")
-    if g.connected():
-        print("All nodes visited.\n")
-    for namespace in g.namespaces():
-        if "parliament.uk" in namespace[1]:
-            print(namespace[0], ":", namespace[1])
+#     g = Graph()
+#     g.parse(target)
+#     for subj, pred, obj in g:
+#         if (subj, pred, obj) not in g:
+#             print("problem with graph in {target}\n")
+#     print(f"{len(g)} statements in {target}.\n")
+#     if g.connected():
+#         print("All nodes visited.\n")
+#     for namespace in g.namespaces():
+#         if "parliament.uk" in namespace[1]:
+#             print(namespace[0], ":", namespace[1])
     for colon in file_to_colons(target):
         if colon not in sorted_flat_list:
             print(f"\n*** {colon} not recognised in {target}\n")
