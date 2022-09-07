ccddc981b4c1cc84d21e341b8ee66d6949884817
Robert Brook <robertbrook@fastmail.fm>
2022-09-07 17:45:16 +0100

    prolog faff

--- meta/generate-docs.pl
+++ meta/generate-docs.pl
@@ -3,9 +3,15 @@
 :- use_module(library(semweb/rdf11)).
 :- use_module(library(semweb/turtle)).
 :- use_module(library(semweb/rdf_portray)).
-
+:- use_module(library(http/html_write)).
+%:- use_module(library(semweb/rdf_library)).
+%
+%rdf_list_library('.').
+%rdf_list_library.
 main :-
-	rdf_read_turtle('../meta/example-rdf/question-and-answer/commons/full-flow/1-question-tabled.ttl',Triples,[]),
-    rdf_portray_as(prefix:id=label),
-    print_term(Triples,[]).
+	
+rdf_read_turtle('../bill/bill-ontology.ttl',Triples,[]),
+ rdf_portray_as(prefix:id=label),
+   print_term(Triples,[]),
 
+rdf([],[],[]).
