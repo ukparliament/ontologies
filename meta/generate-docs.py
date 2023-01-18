27f9a790ad64e64e919de2511880171b22fd7afb
Robert Brook <robertbrook@fastmail.fm>
2023-01-18 21:34:47 +0000

    fix index path

--- meta/generate-docs.py
+++ meta/generate-docs.py
@@ -188,12 +188,6 @@ for ttlpath in ttlfiles:
                 namespaceObject = {"p": p, "n": n}
                 namespaces.append(namespaceObject)
 
-            # try:
-            #     os.makedirs(htmldir + str(ttlpath.parent))
-            #     print(" Made dir " + htmldir + str(ttlpath.parent))
-            # except FileExistsError:
-            #     pass
-
             htmlpath = htmldir + ttlpath.stem + ".html"
             print(htmlpath)
             rel_canonical = (
